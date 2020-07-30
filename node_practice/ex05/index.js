9111472269de3bacb1ea641993035631e3aca70d
fengjiao <lvfe@tcd.ie>
2020-07-31 07:47:18 +0800

    node practice

--- node_practice/ex05/index.js
+++ node_practice/ex05/index.js
@@ -1,6 +1,14 @@
 const {EventEmitter} = require('events')
 module.exports = class Connection {
     // ##BEGIN## 代码已加密
-    
+    constructor(){
+        this.emitter = new EventEmitter();
+    }
+    connection(msg){
+        this.emitter.emit('connection', msg);
+    }
+    onConn(cb){
+        this.emitter.on('connection', cb);
+    }
     // ##END##
 }
